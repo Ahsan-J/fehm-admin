8f499c04e5e1c57b44a3ab53958e1c17d1c3677f
Ahsan-J <ahsan.ahmed99@ymail.com>
2022-02-07 20:24:12 +0500

    moved users to user for simple routes

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -3,27 +3,29 @@ import { useRouter } from "next/router";
 import { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getUserDetail } from "../../api/user";
-import styles from '../../styles/users/user_detail.module.css'
+import styles from '../../styles/user/user_detail.module.css'
 
 const UserDetails: NextPage = () => {
     const router = useRouter();
     const dispatch = useDispatch();
     const { id } = router.query;
 
-    const getDetail = useCallback(async () => {
+    const getDetail = useCallback(async (userId) => {
         try {
             const params = {
-                path: `user/${id}`
+                path: `user/${userId}`
             }
             await dispatch(getUserDetail(params))
         } catch (e) {
             console.log(e)
         }
-    }, [id, dispatch])
+    }, [dispatch])
 
     useEffect(() => {
-        getDetail()
-    }, [getDetail])
+        if(id) {
+            getDetail(id)
+        }
+    }, [getDetail, id])
 
     return (
         <div className={styles.userDetail__container}>
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,20 +1,23 @@
 import moment from "moment";
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Button, Modal, Table } from 'spidev-react-elements';
 import { IModalRef } from "spidev-react-elements/lib/components/Modal/Modal";
 import { getUsers } from "../../api/user";
-import CreateUser from "../../components/users/CreateUser/CreateUser";
+import CreateUser from "../../components/user/CreateUser/CreateUser";
+import { MemberShip, UserRole, UserStatus } from "../../contant/user.enum";
 import { IUser } from "../../model/user";
 import { AppThunkDispatch } from "../../redux/types";
-import styles from '../../styles/users/users.module.css';
+import styles from '../../styles/user/users.module.css';
 
 const Users: NextPage = () => {
     const dispatch = useDispatch<AppThunkDispatch>();
     const [users, setUsers] = useState<Array<IUser>>([]);
     const modalRef = useRef<IModalRef>(null);
+    const router = useRouter();
 
     const getAllUsers = useCallback(async () => {
         try {
@@ -28,25 +31,33 @@ const Users: NextPage = () => {
 
     useEffect(() => {
         getAllUsers();
-    }, [getAllUsers])
+    }, [getAllUsers]);
+
+    const onRowItemClick = useCallback((user) => {
+        router.push(`/user/${user.id}`)
+    }, [router]);
+
+    const onDelete = useCallback((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.stopPropagation();
+    },[]);
+
+    const onEdit = useCallback((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.stopPropagation();
+    },[]);
 
     const columns = useMemo(() => {
         return [
             {
                 label: "ID",
-                keyIndex: "id",
-                render: (v: any) => (
-                    <Link href={`/users/${v}`}>{v}</Link>
-                )
+                keyIndex: "id,email",
+                render: (v: any, _: any, index :any) => `${index + 1}`,
             },
             {
-                label: "First Name",
-                keyIndex: "first_name",
+                label: "Name",
+                keyIndex: "first_name,last_name",
+                render: (v: any) => `${v.first_name} ${v.last_name}`.trim(),
             },
-            {
-                label: "Last Name",
-                keyIndex: "last_name",
-            },
+            
             {
                 label: "Email",
                 keyIndex: "email",
@@ -58,22 +69,35 @@ const Users: NextPage = () => {
             {
                 label: "Role",
                 keyIndex: "role",
+                render: (v: any) => UserRole[v]
             },
             {
                 label: "Status",
                 keyIndex: "status",
+                render: (v: any) => UserStatus[v]
             },
             {
                 label: "Membership",
                 keyIndex: "membership_status",
+                render: (v: any) => MemberShip[v]
             },
             {
                 label: "Created At",
                 keyIndex: "created_at",
                 render: (v: any) => moment(v).format("DD-MMM-YYYY")
             },
+            {
+                label: "Actions",
+                keyIndex: "id",
+                render: (v:any) => (
+                        <div className="table__actionContainer">
+                            <Button iconName="trash" type="danger" onClick={onDelete}/>
+                            <Button iconName="pencil-square" onClick={onEdit} />
+                        </div>
+                    )
+            }
         ];
-    }, []);
+    }, [onEdit, onDelete]);
 
     return (
         <div className={styles.userList__container}>
@@ -85,6 +109,7 @@ const Users: NextPage = () => {
             </div>
             <div className="table_card">
                 <Table
+                    onRowItemClick={onRowItemClick}
                     data={users}
                     columnHeadings={columns} />
             </div>
