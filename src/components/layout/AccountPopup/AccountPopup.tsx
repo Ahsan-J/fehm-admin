179c5f055cca422d819b3c895cf62273c70a8caf
Ahsan-J <ahsan.ahmed99@ymail.com>
2022-04-24 18:34:03 +0500

    Type def updates

--- src/components/layout/AccountPopup/AccountPopup.tsx
+++ src/components/layout/AccountPopup/AccountPopup.tsx
@@ -3,9 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { Button, Icon, Avatar } from "spidev-react-elements";
 import { setAuthUser } from "../../../redux/actions/auth";
 import styles from "./accountpopup.module.css";
-import { RootState } from "../../../redux/types";
+import { AppThunkDispatch, RootState } from "../../../redux/types";
 import Link from "next/link";
 import { logout } from "../../../api/auth";
+import { IUser } from "../../../model/user";
 
 type NavListItem = {
   route?: string;
@@ -24,9 +25,9 @@ export interface IAccountPopupInstance {
 }
 
 const AccountPopup = React.forwardRef<IAccountPopupInstance, propTypes>((props: React.PropsWithChildren<propTypes>, ref) => {
-  const [show, showPopup] = useState(false);
-  const dispatch = useDispatch();
-  const user = useSelector((store: RootState) => store.auth.user);
+  const [show, showPopup] = useState<boolean>(false);
+  const dispatch = useDispatch<AppThunkDispatch>();
+  const user: IUser = useSelector((store: RootState) => store.auth.user);
 
   const onLogout = useCallback(async () => {
     await dispatch(logout())
@@ -78,9 +79,9 @@ const AccountPopup = React.forwardRef<IAccountPopupInstance, propTypes>((props: 
       {show ? (
         <div className={styles.container}>
           <div className={styles.profileDetail}>
-            <Avatar name="Ahsan Ahmed" />
-            <h4>{user.emailuser_name}</h4>
-            <p>{user.emailuser_email}</p>
+            <Avatar name={`${user.first_name} ${user.last_name}`} />
+            <h4>{`${user.first_name} ${user.last_name}`}</h4>
+            <p>{user.email}</p>
           </div>
           <div className={styles.accountNavList}>{renderActions}</div>
           <Button className={styles.logout} onClick={onLogout}>
